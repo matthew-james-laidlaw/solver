dda1100df9c4460d28fe28439e48c5088756d224
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2026-08-16 11:10:21 -0700

    libsolver -> solver, solver -> solve

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,10 +1,10 @@
 mkdir -p build/coverage
 
-llvm-profdata merge -sparse build/libs/libsolver/default.profraw \
+llvm-profdata merge -sparse build/libs/solver/default.profraw \
     -o build/coverage/coverage.profdata
 
 llvm-cov show \
-    build/bin/Debug/libsolver_tests \
+    build/bin/Debug/solver_tests \
     -instr-profile=build/coverage/coverage.profdata \
     -format=html \
     -output-dir=build/coverage
