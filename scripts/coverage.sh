fd6e30ae2670f9f79d66dbe67283f83762394698
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2026-08-16 11:20:03 -0700

    update coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,10 +1,19 @@
 mkdir -p build/coverage
 
+# create a .profdata file
 llvm-profdata merge -sparse build/libs/solver/default.profraw \
     -o build/coverage/coverage.profdata
 
-llvm-cov show \
-    build/bin/Debug/solver_tests \
+# report coverage to stdout
+llvm-cov report                                     \
+    build/bin/Debug/solver_tests                    \
     -instr-profile=build/coverage/coverage.profdata \
-    -format=html \
-    -output-dir=build/coverage
+    -ignore-filename-regex='.*/build/_deps/.*'
+
+# also save coverage to an html file
+llvm-cov show                                       \
+    build/bin/Debug/solver_tests                    \
+    -instr-profile=build/coverage/coverage.profdata \
+    -format=html                                    \
+    -output-dir=build/coverage                      \
+    -ignore-filename-regex='.*/build/_deps/.*'
