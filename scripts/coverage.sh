372d49ae09dac9ab66b521cc947f24e0ac53c111
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2026-08-16 10:38:17 -0700

    generator stuff

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,7 +4,7 @@ llvm-profdata merge -sparse build/libs/libsolver/default.profraw \
     -o build/coverage/coverage.profdata
 
 llvm-cov show \
-    build/bin/libsolver_tests \
+    build/bin/Debug/libsolver_tests \
     -instr-profile=build/coverage/coverage.profdata \
     -format=html \
     -output-dir=build/coverage
